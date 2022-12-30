c9d0aaa27d1800a255c11e88d24221c7d764bb9b
Maghnus O Donnell <g00377514@atu.ie>
2022-12-30 23:15:03 +0000

    updaed interrogation to now exit after last button press

--- Assets/Scripts/suspects/SusDialogue.cs
+++ Assets/Scripts/suspects/SusDialogue.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SusDialogue : MonoBehaviour
 {
     public Dialogue dialogue;
-    public Camera camera;
+    new Camera camera;
 
     public GameObject kit;
 
@@ -16,13 +16,26 @@ public class SusDialogue : MonoBehaviour
     Ray ray;
     float ray_length = 3f;
 
-    int i = 0;
+
+    /// <summary>
+    ///  i is used to pass array element to be displayed to dialogueManager.
+    ///  buttons_pressed is amount of buttons pressed by character.
+    ///  max_button_presses is how many buttons the player can press before interrogation is set is inactive. default is 3 due to current interrogation dialogue only containing 3 options
+    ///  before exiting. this will need to be changed on a character-by-character basis if dialogue extends beyond 3 options
+    /// </summary>
+
+    int i = 0, buttons_pressed;
+
+    public int max_button_presses = 3;
 
 
     void Awake()
     {
         kit.SetActive(false);
         this.gameObject.GetComponent<SusDialogue>().enabled = false;
+
+        //  references child camera gameobject in the kit gameobject under camera. This is used for reading the buttons pressed
+        camera = kit.GetComponentInChildren<Camera>();
     }
 
     void Update()
@@ -31,36 +44,41 @@ public class SusDialogue : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, ray_length) && Input.GetButtonDown("Fire1"))
         {
-            if(i <= 8)
+
+            switch (hit.collider.gameObject.name)
             {
-                switch (hit.collider.gameObject.name)
-                {
-                    case "button1":
-                        TriggerDialogue(i);
-                        i += 3;
+                case "button1":
+                    TriggerDialogue(i);
+                    buttons_pressed++;
+                    i += 3;
 
-                        break;
-                    case "button2":
-                        TriggerDialogue(i + 1);
-                        aggression += 1;
-                        i += 3;
+                    break;
+                case "button2":
+                    TriggerDialogue(i + 1);
+                    aggression += 1;
+                    buttons_pressed++;
+                    i += 3;
 
-                        break;
-                    case "button3":
-                        TriggerDialogue(i + 2);
-                        aggression += 2;
-                        i += 3;
+                    break;
+                case "button3":
+                    TriggerDialogue(i + 2);
+                    aggression += 2;
+                    buttons_pressed++;
+                    i += 3;
 
-                        break;
-                    default:    Debug.Log("Raycast hit object: " + hit.collider.gameObject.name);
-                        break;
-                }
-
+                    break;
+                default:
+                    Debug.Log("Raycast hit object: " + hit.collider.gameObject.name);
+                    break;
             }
-            else
+
+
+
+            if (buttons_pressed == max_button_presses)
             {
                 Debug.Log("FINISHED INTERROGAION");
                 Invoke("endInterrogation", 5f);
+
             }
         }
     }
