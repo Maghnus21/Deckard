0ba4df84762e188e68a80203a391e43458765a95
Maghnus O Donnell <g00377514@atu.ie>
2023-01-18 23:27:55 +0000

    updated rebarBehaviour to now parent object upon collision with enemy

--- Assets/Scripts/weapons/bullet/RebarBehaviour.cs
+++ Assets/Scripts/weapons/bullet/RebarBehaviour.cs
@@ -6,6 +6,10 @@ public class RebarBehaviour : MonoBehaviour
 {
     public LayerMask obj_layer;
     public GameObject bar;
+    public float damage = 45f;
+
+    //  ensures object only parents once with hit object and not upon collision with passing objects
+    bool is_parented = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +25,40 @@ public class RebarBehaviour : MonoBehaviour
         {
             Debug.Log("HIT COLLIDER");
 
+            if (!is_parented)
+            {
+                gameObject.transform.parent = collision.transform;
+                is_parented = true;
+            }
+
             this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
             this.gameObject.GetComponent<Collider>().enabled = false;
 
             bar.GetComponent<CapsuleCollider>().enabled = true;
+        }
+
+        if (collision.gameObject.GetComponent<Collider>() == true && collision.gameObject.GetComponentInParent<enemyHealth>())
+        {
+            Debug.Log("HIT COLLIDER");
+
+            if (!is_parented)
+            {
+                gameObject.transform.parent = collision.transform;
+                is_parented = true;
+            }
+
+            this.gameObject.GetComponent<Collider>().enabled = false;
+
+            if(collision.gameObject.GetComponentInParent<enemyHealth>().health > 0)
+            {
+                collision.gameObject.GetComponentInParent<enemyHealth>().health -= damage;
+            }
+
+            if(collision.gameObject.GetComponentInParent<enemyHealth>().health <= 0)
+            {
+                collision.gameObject.GetComponentInParent<enemyHealth>().EnemyDeath();
+            }
+
         }
     }
 
