ca013c6a87a35fe6129d4831417409fcd9864430
Maghnus O Donnell <g00377514@atu.ie>
2023-02-08 12:19:45 +0000

    updated lift model and operation

--- Assets/Scripts/LiftButtons.cs
+++ Assets/Scripts/LiftButtons.cs
@@ -5,39 +5,31 @@ using UnityEngine;
 public class LiftButtons : MonoBehaviour
 {
     public GameObject lift;
-    public Vector3 lift_w_position;
-    public float speed = 0.1f;
-
-    private Coroutine lift_animation;
+    public Transform destination;
+    public float speed = 10f;
 
     bool reached_des = true;
 
     private void Awake()
     {
-        lift_w_position = new Vector3(lift.transform.position.x, lift_w_position.y, lift.transform.position.z);
+        
     }
+
+    private void Update()
+    {
+        if (!reached_des)
+        {
+            lift.transform.position = Vector3.MoveTowards(lift.transform.position, destination.position, speed * Time.deltaTime);
+        }
+        if(lift.transform.position == destination.position)
+        {
+            reached_des = true;
+        }
+    }   
 
     public void LiftMovement()
     {
-        
-        lift_animation = StartCoroutine(liftMove(lift_w_position, speed));
-        
-    }
-
-    private IEnumerator liftMove(Vector3 target_pos, float speed)
-    {
         reached_des = false;
-
-        Vector3 start_pos = lift.transform.position;
-
-        float time = 0f;
-        while(time < speed)
-        {
-            lift.transform.position = Vector3.Lerp(start_pos, target_pos, time / speed);
-            time += Time.deltaTime;
-            yield return null;
-        }
-        lift.transform.position = target_pos;
     }
 
 }
