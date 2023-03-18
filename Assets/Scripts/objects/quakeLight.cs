d8c963f837ecec651012ad7ad3446d345cab846d
Maghnus O Donnell <g00377514@atu.ie>
2023-03-18 20:07:38 +0000

    added new textures and applied to meshes

--- Assets/Scripts/objects/quakeLight.cs
+++ Assets/Scripts/objects/quakeLight.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class quakeLight : MonoBehaviour
 {
